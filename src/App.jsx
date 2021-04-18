0a58a07d6e9b53a04f64e563bab94376e96cda49
JackMechem <mechemjack@gmail.com>
2021-04-18 11:55:54 -0700

    Changed file structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,14 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Switch, Redirect } from 'react-router-dom';
-import PageNotFound from './pages/PageNotFound.jsx';
-import MainPage from './pages/MainPage';
-import program from './pages/program'
-import space from './pages/space'
-import Pano1 from './pages/panos/Pano1';
-import Pano2 from './pages/panos/Pano2';
-import Pano3 from './pages/panos/Pano3';
-import Pano4 from './pages/panos/Pano4';
-import Pano5 from './pages/panos/Pano5';
-import Pano6 from './pages/panos/Pano6';
-import Pano7 from './pages/panos/Pano7';
-import Pano8 from './pages/panos/Pano8';
-import ViewModel from './pages/comps/modelView/ViewModel';
+import PageNotFound from './comps/PageNotFound';
+import MainPage from './comps/MainPage';
+import program from './comps/program'
+import space from './comps/space'
+import Pano1 from './comps/Pano1';
+import Pano2 from './comps/Pano2';
+import Pano3 from './comps/Pano3';
+import Pano4 from './comps/Pano4';
+import Pano5 from './comps/Pano5';
 
 const App = () => {
 
@@ -28,9 +24,6 @@ const App = () => {
         <Route exact path={"/Pano3"} component={Pano3} />
         <Route exact path={"/Pano4"} component={Pano4} />
         <Route exact path={"/Pano5"} component={Pano5} />
-        <Route exact path={"/Pano6"} component={Pano6} />
-        <Route exact path={"/Pano7"} component={Pano7} />
-        <Route exact path={"/Pano8"} component={Pano8} />
         <Route exact path={"/404"} component={PageNotFound} />
         <Redirect to="/404" />
       </Switch>
